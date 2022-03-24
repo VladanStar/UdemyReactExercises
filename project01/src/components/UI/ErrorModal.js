075e662d442a1f3da697a8abc77f416842128a62
VladanStar <vladancupric@gmail.com>
2022-03-24 14:05:27 +0100

    New commits

---
+++ project01/src/components/UI/ErrorModal.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Card from "./Card";
+import Button from "./Button";
+
+const ErrorModal = (props) => {
+  return (
+    <Card>
+      <header>
+        <h2>{props.title}</h2>
+      </header>
+      <div>
+        <p>{props.message}</p>
+      </div>
+      <footer>
+        <Button>Close</Button>
+      </footer>
+    </Card>
+  );
+};
+
+export default ErrorModal;
