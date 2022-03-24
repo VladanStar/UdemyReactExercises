3780e4480b6d394d062556e1abc10683b80d8c80
VladanStar <vladancupric@gmail.com>
2022-03-24 09:52:02 +0100

    New commits

--- project01/src/components/Players/AddPlayer.js
+++ project01/src/components/Players/AddPlayer.js
@@ -1,25 +1,24 @@
-import React from 'react'
+import React from "react";
+import Card from "../UI/Card";
+import classes from "./AddPlayer.module.css";
 
 const AddPlayer = (props) => {
-
-    const addPlayerHandler = (event) => {
-        event.preventDefault();
-
-
-    }
-    return (
-      <Card>
+  const addPlayerHandler = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <Card clasName={classes.input}>
       <form onSubmit={addPlayerHandler}>
-          <label htmlFor='playername'>Player name</label>
-          <input type='text' id='playername' />
-          <label id='age'>Age</label>
-          <input type='number' htmlFor='age'/>
-          <label htmlFor='position'>Position</label>
-          <input id='position' type='text' />
-          <button type='submit'>Add Player</button>
-            </form>
-            </Card>
-  )
-}
+        <label htmlFor="playername">Player name</label>
+        <input type="text" id="playername" />
+        <label id="age">Age</label>
+        <input type="number" htmlFor="age" />
+        <label htmlFor="position">Position</label>
+        <input id="position" type="text" />
+        <button type="submit">Add Player</button>
+      </form>
+    </Card>
+  );
+};
 
-export default AddPlayer
+export default AddPlayer;
